6e35aea3bea786431e95720c8176a23ef8c8d2a9
adayevkp <adayevkp@gmail.com>
2021-01-19 19:35:11 +0300

    remove all old files

--- backend/src/com/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/',               admin.site.urls),
-    path('api/v1/task_tracker/', include('task_tracker.urls')),
-    path('api/v1/auth/',         include('rest_framework.urls')),
-]
