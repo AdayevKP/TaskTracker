bfe72b777a3a9efb24263b067c29ce6decf619c9
adayevkp <adayevkp@gmail.com>
2021-02-03 20:23:17 +0300

    Remove old files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,36 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';  
 import * as serviceWorker from './tools/serviceWorker';
-import TaskTracker from './taskTrackerApp/taskTracker'
-import LoginPage from './auth/loginPage'
 import AppRouter from './routes'
  
-/*
-class App extends Component {
-  checkAuthorization(func){
-    if (this.authorized) {
-      return func;
-    } else {
-      return (..._) => (<Redirect to='/login' />);
-    }
-  }
 
-  taskTrackerComponent(){
-    return this.checkAuthorization(this.taskTarckerApp.ui.render)()
-  }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <Route exact path={this.taskTarckerApp.path} component={this.taskTrackerComponent} />
-          <Route exact path={this.auth.path} component={this.auth.ui.render} />
-        </div>
-      </Router>
-    );
-  }
-}
-*/
 class App extends Component{
     render() {
         return (
--- frontend/src/tools/bindMethods.js
+++
@@ -1,8 +0,0 @@
-export default function bindMethods(instance) {
-    const names = Object.getOwnPropertyNames(instance.__proto__)
-    for (let name of names) {
-        if (typeof instance[name] == 'function') {
-            instance[name] = instance[name].bind(instance)
-        }
-    }
-}
